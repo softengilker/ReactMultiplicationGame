905cdb03e89b0dead1dc227f6f8c35ff833dec0e
softengilker <ilkerkonar@gmail.com>
2020-04-18 12:16:55 +0300

    Updates

---
+++ precache-manifest.e7db28338d93b0f6a6e97f99b72ba6ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29080ee2914b82d43290585789cd4ff9",
+    "url": "/softengilker/ReactMultiplicationGame/index.html"
+  },
+  {
+    "revision": "f13b7893d7952f557af9",
+    "url": "/softengilker/ReactMultiplicationGame/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "ad0c497024ffff7eb3c6",
+    "url": "/softengilker/ReactMultiplicationGame/static/css/main.07bb31c9.chunk.css"
+  },
+  {
+    "revision": "f13b7893d7952f557af9",
+    "url": "/softengilker/ReactMultiplicationGame/static/js/2.ab04b366.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/softengilker/ReactMultiplicationGame/static/js/2.ab04b366.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ad0c497024ffff7eb3c6",
+    "url": "/softengilker/ReactMultiplicationGame/static/js/main.66cd319c.chunk.js"
+  },
+  {
+    "revision": "23da72d85a9148107398",
+    "url": "/softengilker/ReactMultiplicationGame/static/js/runtime-main.a35a39d7.js"
+  }
+]);
